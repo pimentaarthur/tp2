cb7684bcbdbd3401de01c763cae8c3e113a12ffd
Arthur Morais Pimenta <112594330+pimentaarthur@users.noreply.github.com>
2024-04-11 10:59:43 -0300

    Add files via upload

---
+++ tp2/code/Trabalho.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Trabalho {
+	        public static void main(String args[]) throws IOException {
+
+            String path = "D:\\users\\1349019\\Desktop\\tp2\\characters.csv";
+            leitor(path);
+        }
+    
+    
+	   public static void leitor(String path) throws IOException {
+		BufferedReader buffRead = new BufferedReader(new FileReader(path));
+		String linha = "";
+		while (true) {
+			if (linha != null) {
+				System.out.println(linha);
+
+			} else
+				break;
+			linha = buffRead.readLine();
+		}
+		buffRead.close();
+	}
+
+}
